ee8af975e320b0ffd723585df97b86678575cbb0
Anthesy <115452668+Anthesy@users.noreply.github.com>
2025-01-25 20:45:46 +0100

    wagons integration

--- _Intensifs-2025 (Dossier Unity)/Assets/_Pauline/Scripts/Script_CreateSpline.cs
+++ _Intensifs-2025 (Dossier Unity)/Assets/_Pauline/Scripts/Script_CreateSpline.cs
@@ -1,14 +1,12 @@
 using UnityEngine;
 using UnityEngine.Splines;
-using System.Collections; // Pour utiliser les coroutines
+using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using Unity.VisualScripting;
 
 public class TrainsSplineController : MonoBehaviour
 {
-
     public GameObject Locomotive;
     public GameObject Wpassager;
     public GameObject Wressource;
@@ -16,41 +14,35 @@ public class TrainsSplineController : MonoBehaviour
     public static SplineContainer Path;
 
     private List<MoveAlongSpline> trains;
+    private bool isInstantiatingPassager = false;
+    private bool isInstantiatingRessource = false;
     
     void Start()
     {
         if (!Application.isPlaying) return;
         
-        // Assure que le SplineContainer est pr�sent
         Path = GetComponent<SplineContainer>() ?? gameObject.AddComponent<SplineContainer>();
         Path.Spline.Clear();
         
         trains = new List<MoveAlongSpline>();
-
-        // D�marrer la coroutine pour ajouter un autre wagon apr�s 0.1 seconde
-        //StartCoroutine(InstantiateWagonAfterDelay(1f));
     }
 
     void AddAutoSmoothSplinePoint(Spline spline, Vector3 position)
     {
         var knot = new BezierKnot(position);
         spline.Add(knot);
-
-        // Set les points de la spline en "Auto" cela les rend plus smooth
         spline.SetTangentMode(spline.Count - 1, TangentMode.AutoSmooth);
     }
 
     public void TryCreateSpline(StationCollision station)
     {
         Spline currentSpline = new Spline();
-        
         bool isPathComplete = false;
         GameObject path = station.Connection;
+
         while (path)
         {
-            Debug.Log("PATH: " + path);
             RailPath rail = path.GetComponentInParent<RailPath>();
-
             if (!rail)
             {
                 isPathComplete = true;
@@ -63,11 +55,11 @@ public class TrainsSplineController : MonoBehaviour
                 {
                     foreach (var point in pathConnector.points)
                         AddAutoSmoothSplinePoint(currentSpline, point.transform.position);
-                    path = pathConnector.points[pathConnector.points.Count - 1].GetComponent<RailCollisions>().Connection;
+                    path = pathConnector.points[^1].GetComponent<RailCollisions>().Connection;
                     break;
                 }
                 
-                if (pathConnector.points[pathConnector.points.Count - 1] == path)
+                if (pathConnector.points[^1] == path)
                 {
                     for (int i = pathConnector.points.Count - 1; i >= 0; i--)
                         AddAutoSmoothSplinePoint(currentSpline, pathConnector.points[i].transform.position);
@@ -79,33 +71,23 @@ public class TrainsSplineController : MonoBehaviour
 
         if (!isPathComplete) return;
         
-        Debug.Log("Path complete: " + currentSpline.Count);
-
         foreach (var spline in Path.Splines)
             if (DoesSplineContains(spline, station.Connection.GetComponent<RailCollisions>()))
                 return;
         
         Path.AddSpline(currentSpline);
-
-        Debug.Log("spline Lenght : " + (Path.Splines.Count-1));
         
-        if (Path.Splines.Count-1 == trains.Count) return;
+        if (Path.Splines.Count - 1 == trains.Count) return;
 
         MoveAlongSpline train = Instantiate(Locomotive).GetComponent<MoveAlongSpline>();
         train.SetSpline(currentSpline);
         trains.Add(train);
 
         if (currentSpline.GetLength() > 20)
-        {
-            StopCoroutine(InstantiateWagonAfterDelay(0.8f,true, currentSpline));
-            StartCoroutine(InstantiateWagonAfterDelay(0.8f,true, currentSpline));
-        }
-
+            StartCoroutine(InstantiateWagonAfterDelay(0.78f, true, currentSpline));
+        
         if (currentSpline.GetLength() > 40)
-        {
-            StopCoroutine(InstantiateWagonAfterDelay(1.6f,false, currentSpline));
-            StartCoroutine(InstantiateWagonAfterDelay(1.6f,false, currentSpline));
-        }
+            StartCoroutine(InstantiateWagonAfterDelay(1.3f, false, currentSpline));
     }
     
     public void TryRemoveSpline(RailCollisions rail)
@@ -118,47 +100,47 @@ public class TrainsSplineController : MonoBehaviour
             if (DoesSplineContains(Path.Splines[i], rail))
             {
                 splineToRemove.Add(Path.Splines[i]);
-                trainToRemove.Add(trains[i-1]);
+                trainToRemove.Add(trains[i - 1]);
             }
         }
 
         for (int i = 0; i < splineToRemove.Count; i++)
         {
-            MoveAlongSpline[] wagons = FindObjectsByType<MoveAlongSpline>(FindObjectsSortMode.None);
-            foreach (var wagon in wagons)
+            foreach (var wagon in FindObjectsByType<MoveAlongSpline>(FindObjectsSortMode.None))
             {
-                if(wagon.GetSpline().Equals(splineToRemove[i]))
+                if (wagon.GetSpline().Equals(splineToRemove[i]))
                     Destroy(wagon.gameObject);
             }
             Path.RemoveSpline(splineToRemove[i]);
             Destroy(trainToRemove[i].gameObject);
             trains.Remove(trainToRemove[i]);
         }
-        
-        Debug.Log("Spline to remove count: " + splineToRemove.Count);
     }
 
     private bool DoesSplineContains(Spline spline, RailCollisions rail)
     {
         foreach (var knot in spline.Knots)
         {
-            Vector3 knotPos = new Vector3(knot.Position.x, knot.Position.y, knot.Position.z);
-            if ((rail.transform.position - knotPos).magnitude < 0.1f)
+            if ((rail.transform.position - (Vector3)knot.Position).magnitude < 0.1f)
                 return true;
         }
-
         return false;
     }
 
-    // Coroutine pour instancier un wagon apr�s un d�lai
     IEnumerator InstantiateWagonAfterDelay(float delay, bool passager, Spline spline)
     {
-        yield return new WaitForSeconds(delay); // Attendre pendant le d�lai
-        Debug.Log("WAGON " + passager);
+        if ((passager && isInstantiatingPassager) || (!passager && isInstantiatingRessource))
+            yield break;
 
-        // Instancier un autre wagon
+        if (passager) isInstantiatingPassager = true;
+        else isInstantiatingRessource = true;
+
+        yield return new WaitForSeconds(delay);
+
         MoveAlongSpline wagon = Instantiate(passager ? Wpassager : Wressource).GetComponent<MoveAlongSpline>();
         wagon.SetSpline(spline);
 
+        if (passager) isInstantiatingPassager = false;
+        else isInstantiatingRessource = false;
     }
 }
