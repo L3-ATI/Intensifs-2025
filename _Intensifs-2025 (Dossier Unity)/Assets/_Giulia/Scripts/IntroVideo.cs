96a6c0155fab75465d9c73b6c5f9b7d344bd02c0
Giu <giulia.perucca@gmail.com>
2025-01-26 20:54:45 +0100

    Jeu presque fini !

--- _Intensifs-2025 (Dossier Unity)/Assets/_Giulia/Scripts/IntroVideo.cs
+++ _Intensifs-2025 (Dossier Unity)/Assets/_Giulia/Scripts/IntroVideo.cs
@@ -14,14 +14,11 @@ public class IntroVideo : MonoBehaviour
     {
         videoPlayer = GetComponent<VideoPlayer>();
 
-        // S'abonner à l'événement de fin de la vidéo
         videoPlayer.loopPointReached += OnVideoFinished;
 
-        // Masquer le logo au début
         logoImage.gameObject.SetActive(false);
     }
 
-    // Méthode appelée quand la vidéo se termine
     void OnVideoFinished(VideoPlayer vp)
     {
         StartCoroutine(ShowLogoAndFade());
@@ -29,7 +26,6 @@ public class IntroVideo : MonoBehaviour
 
     private IEnumerator ShowLogoAndFade()
     {
-        // Activer et afficher le logo
         logoImage.gameObject.SetActive(true);
         logoImage.canvasRenderer.SetAlpha(0.0f); // Initialiser la transparence à 0
         logoImage.CrossFadeAlpha(1.0f, 0.5f, false); // Fade in sur 0.5 seconde
